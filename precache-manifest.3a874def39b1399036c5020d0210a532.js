1b80f330a2584b5f60ffc9d6ab0926e89a18e80d
GRILLO-Benjamin <benjisensei@gmail.com>
2022-10-15 15:54:33 +0200

    Updates

---
+++ precache-manifest.3a874def39b1399036c5020d0210a532.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca50ab7ef284896ca056",
+    "url": "/CV-ENGLISH-2022/static/css/main.1dc26dcf.chunk.css"
+  },
+  {
+    "revision": "ca50ab7ef284896ca056",
+    "url": "/CV-ENGLISH-2022/static/js/main.ca50ab7e.chunk.js"
+  },
+  {
+    "revision": "d17e16748c0fa7aba96d",
+    "url": "/CV-ENGLISH-2022/static/js/1.d17e1674.chunk.js"
+  },
+  {
+    "revision": "775da1e49b44ceee3575",
+    "url": "/CV-ENGLISH-2022/static/js/runtime~main.775da1e4.js"
+  },
+  {
+    "revision": "fcbeea198c355e49ec2035df2cb5375b",
+    "url": "/CV-ENGLISH-2022/index.html"
+  }
+];
